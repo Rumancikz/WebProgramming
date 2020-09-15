eabb642af758f9c969dd9dc859334a10a5b95db3
Rumancikz <68083947+Rumancikz@users.noreply.github.com>
2020-09-14 22:34:43 -0400

    Zach Nav update and icons

---
+++ lottery.js
@@ -0,0 +1,32 @@
+function getRandomSixDigitNumber () {
+    const randomSixDigitNumber = 
+    Math.floor(Math.random() * 8 + 1).toString() +
+    Math.floor(Math.random() * 9).toString() +
+    Math.floor(Math.random() * 9).toString() +
+    Math.floor(Math.random() * 9).toString() +
+    Math.floor(Math.random() * 9).toString() +
+    Math.floor(Math.random() * 8 + 1).toString()
+    return randomSixDigitNumber
+}
+let arrGuesses
+let guessCorrectly = false
+let numGuesses = 0
+while (guessCorrectly === false) {
+const LottoNumber = getRandomSixDigitNumber()
+let numberOfGuesses = 0
+let guesses = []
+    while (numberOfGuesses < 10){
+        numberOfGuesses += 1
+        const guess = getRandomSixDigitNumber()
+        guess === LottoNumber ?  guessCorrectly = true : guessCorrectly = false
+        guesses = [...guesses, guess]
+    }
+    guessCorrectly === true ? arrGuesses = [...guesses] : null
+    guessCorrectly === true ? numGuesses = numberOfGuesses : null
+}
+console.log(`It took the computer ${numGuesses} guesses to win the lottery!`);
+console.log(arrGuesses);
+
+
+
+
