486adab71c57548de275108305a3c84ad8c34b26
Rumancikz <68083947+Rumancikz@users.noreply.github.com>
2020-09-16 20:08:32 -0400

    kinda finished my side for now

--- lottery.js
+++ lottery.js
@@ -8,24 +8,34 @@ function getRandomSixDigitNumber () {
     Math.floor(Math.random() * 8 + 1).toString()
     return randomSixDigitNumber
 }
+
+
 let arrGuesses
 let guessCorrectly = false
 let numGuesses = 0
-while (guessCorrectly === false) {
+// while (guessCorrectly === false) {
 const LottoNumber = getRandomSixDigitNumber()
 let numberOfGuesses = 0
 let guesses = []
-    while (numberOfGuesses < 10){
-        numberOfGuesses += 1
-        const guess = getRandomSixDigitNumber()
-        guess === LottoNumber ?  guessCorrectly = true : guessCorrectly = false
-        guesses = [...guesses, guess]
-    }
-    guessCorrectly === true ? arrGuesses = [...guesses] : null
-    guessCorrectly === true ? numGuesses = numberOfGuesses : null
+
+while (guessCorrectly === false) {
+    numberOfGuesses += 1
+    const guess = getRandomSixDigitNumber()
+    guess === LottoNumber ?  guessCorrectly = true : guessCorrectly = false
 }
-console.log(`It took the computer ${numGuesses} guesses to win the lottery!`);
-console.log(arrGuesses);
+console.log(`It took the computer ${numberOfGuesses} guesses to win the lottery!`);
+
+//     while (numberOfGuesses < 10){
+//         numberOfGuesses += 1
+//         const guess = getRandomSixDigitNumber()
+//         guess === LottoNumber ?  guessCorrectly = true : guessCorrectly = false
+//         guesses = [...guesses, guess]
+//     }
+//     guessCorrectly === true ? arrGuesses = [...guesses] : null
+//     guessCorrectly === true ? numGuesses = numberOfGuesses : null
+// }
+// console.log(`It took the computer ${numGuesses} guesses to win the lottery!`);
+// console.log(arrGuesses);
 
 
 
