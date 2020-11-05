6c73402aa6f423eda84180b3211bd9db9043d6ec
Rumancikz <68083947+Rumancikz@users.noreply.github.com>
2020-11-05 10:58:12 -0500

    done ani 1 and 2

--- Programming/t1/GroupProject3/script.js
+++ Programming/t1/GroupProject3/script.js
@@ -1,42 +1,86 @@
 //Animation One button and control
+let atStep1 = 0
+let atStep2 = 0
+let atStep3 = 0
+const stepList1 = {
+    "1":[1,2,3,4,5,6],
+    '2':[2,3,4],
+    '3':[5],
+}
+const stepList2 = {
+    "1":[1,2,3,4,5,6,7,8,9,10,11,12,13,14],
+    '2':[2,3,4,5,7,10],
+    '3':[12,13],
+}
+const stepList3 = {
+    "1":[1,2,3,4,5,6,7,8,9,10,11,12],
+    '2':[2,3,5,6,7,10],
+    '3':[8,9],
+}
 const buttonOne = document.querySelector('button.One')
-buttonOne.addEventListener("click", ()=> {animationOne()})
-let animationOneStep1 = 0
-let animationOneStep2 = 0
-let animationOneStep3 = 0
-function animationOne(){
-    animationOneStep1 += 1
-    if (animationOneStep1 < 2){
-        const toStep = document.querySelectorAll(`.One p.one.${CSS.escape(animationOneStep1)}`)        
+buttonOne.addEventListener("click", ()=> {
+    atStep1 += 1
+    if (atStep1 < 7){
+        console.log(atStep1);
+        nextStep(stepList1, atStep1, 'One')
+    }else{
+        atStep1 = 0
+        clearStep('One')
+        clearSee('One')
+    }
+})
+const buttonTwo = document.querySelector('button.Two')
+buttonTwo.addEventListener("click", ()=> {
+    atStep2 += 1
+    if (atStep2 < 15){
+        console.log(atStep2);
+        nextStep(stepList2, atStep2, 'Two')
+    }else{
+        atStep2 = 0
+        clearStep('Two')
+        clearSee('Two')
+    }
+})
+function nextStep(stepList, atStep, animation){ 
+    const step1Arr = stepList['1']
+    const step2Arr = stepList['2']
+    const step3Arr = stepList['3']
+    const toDo1 = step1Arr.includes(atStep)
+    const toDo2 = step2Arr.includes(atStep)
+    const toDo3 = step3Arr.includes(atStep)
+    console.log(step1Arr, step2Arr, step3Arr);
+    console.log(toDo1, toDo2, toDo3);
+    clearStep(animation)
+    if (toDo1 === true){
+        const toStep = document.querySelectorAll(`.${CSS.escape(animation)} .one.${CSS.escape(atStep)}`)        
         toStep.forEach(element => {
             element.classList.add('step')
-        }); 
-        // toStep.classList.add('step')
-    }
-    else if (animationOneStep1 > 1 && animationOneStep1 < 7){
-        
-        let toStep = document.querySelectorAll(`.One .one.${CSS.escape(animationOneStep1)}`)        
-        let toRemove = document.querySelectorAll(`.One .one.${CSS.escape(animationOneStep1 - 1)}`)
-        console.log(toStep);
-        if (animationOneStep1 === 2){
-            animationOneStep2 += 1
-            toStep = document.querySelectorAll(`.One .one.${CSS.escape(animationOneStep1)}, .two.${CSS.escape(animationOneStep2)}`) 
-            toRemove = document.querySelectorAll(`.One .one.${CSS.escape(animationOneStep1)}, .two.${CSS.escape(animationOneStep2 - 1)}`)        
-        }
-        
-        // toStep.classList.add('step')
-        // toRemove.classList.remove('step')
+        });   
+    }    
+    if (toDo2 === true){
+        const toStep = document.querySelectorAll(`.${CSS.escape(animation)} .two.${CSS.escape(atStep)}`)        
         toStep.forEach(element => {
             element.classList.add('step')
-        });        
-        toRemove.forEach(element => {
-            element.classList.remove('step')
-        })
+            element.classList.add('see')
+        });   
     }
-    else{
-        const toRemove = document.querySelector(`p.one.${CSS.escape(animationOneStep1 - 1)}`)        
-        toRemove.classList.remove('step')
-        animationOneStep1 = 0
+    if (toDo3 === true){
+        const toStep = document.querySelectorAll(`.${CSS.escape(animation)} .three.${CSS.escape(atStep)}`)        
+        toStep.forEach(element => {
+            element.classList.add('step')
+            element.classList.add('see')
+        });           
     }
-    
+}
+function clearStep(animation) {
+    const toRemove = document.querySelectorAll(`.${CSS.escape(animation)} .step`)
+    toRemove.forEach(element => {
+        element.classList.remove('step')
+    })
+}
+function clearSee(animation) {
+    const toRemove = document.querySelectorAll(`.${CSS.escape(animation)} .see`)
+    toRemove.forEach(element => {
+        element.classList.remove('see')
+    })
 }
