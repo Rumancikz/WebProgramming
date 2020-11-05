6dda7f4ed0ba76bb56bee0a3e5b627a3aef691ca
Rumancikz <68083947+Rumancikz@users.noreply.github.com>
2020-11-04 21:33:29 -0500

    lol skrew it all

--- Programming/t1/GroupProject3/script.js
+++ Programming/t1/GroupProject3/script.js
@@ -1,20 +1,42 @@
-var elem = document.getElementById('draw-shapes');
-var params = { width: 285, height: 200 };
-var two = new Two(params).appendTo(elem);
-
-// two has convenience methods to create shapes.
-var circle = two.makeCircle(72, 100, 50);
-var rect = two.makeRectangle(213, 100, 100, 100);
-
-// The object returned has many stylable properties:
-circle.fill = '#FF8000';
-circle.stroke = 'orangered'; // Accepts all valid css color
-circle.linewidth = 5;
-
-rect.fill = 'rgb(0, 200, 255)';
-rect.opacity = 0.75;
-rect.noStroke();
-
-// Don't forget to tell two to render everything
-// to the screen
-two.update();
+//Animation One button and control
+const buttonOne = document.querySelector('button.One')
+buttonOne.addEventListener("click", ()=> {animationOne()})
+let animationOneStep1 = 0
+let animationOneStep2 = 0
+let animationOneStep3 = 0
+function animationOne(){
+    animationOneStep1 += 1
+    if (animationOneStep1 < 2){
+        const toStep = document.querySelectorAll(`.One p.one.${CSS.escape(animationOneStep1)}`)        
+        toStep.forEach(element => {
+            element.classList.add('step')
+        }); 
+        // toStep.classList.add('step')
+    }
+    else if (animationOneStep1 > 1 && animationOneStep1 < 7){
+        
+        let toStep = document.querySelectorAll(`.One .one.${CSS.escape(animationOneStep1)}`)        
+        let toRemove = document.querySelectorAll(`.One .one.${CSS.escape(animationOneStep1 - 1)}`)
+        console.log(toStep);
+        if (animationOneStep1 === 2){
+            animationOneStep2 += 1
+            toStep = document.querySelectorAll(`.One .one.${CSS.escape(animationOneStep1)}, .two.${CSS.escape(animationOneStep2)}`) 
+            toRemove = document.querySelectorAll(`.One .one.${CSS.escape(animationOneStep1)}, .two.${CSS.escape(animationOneStep2 - 1)}`)        
+        }
+        
+        // toStep.classList.add('step')
+        // toRemove.classList.remove('step')
+        toStep.forEach(element => {
+            element.classList.add('step')
+        });        
+        toRemove.forEach(element => {
+            element.classList.remove('step')
+        })
+    }
+    else{
+        const toRemove = document.querySelector(`p.one.${CSS.escape(animationOneStep1 - 1)}`)        
+        toRemove.classList.remove('step')
+        animationOneStep1 = 0
+    }
+    
+}
