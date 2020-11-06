74bb7f8a03ff6d7148f1da24111cd0eb9821d82d
Jaggan Ragoonanan <ragooj@pba.edu>
2020-11-05 21:08:58 -0500

    third animation
    zach carried today again

--- Programming/t1/GroupProject3/script.js
+++ Programming/t1/GroupProject3/script.js
@@ -1,7 +1,54 @@
-//Animation One button and control
+//   * Program:  Team_1_P03_11052020 script.js
+
+//   * purpose: Group Project 3 
+
+//   * Objectives: 1) Animating on jQuery
+
+//   * Class: CSC 3323 - Web Programming; Fall 2020; TR: 645PM - 800PM  
+
+//   * Instructor: Dr. Fazelpour - Copyright © 2020  
+
+//   * Programmer(s): Team 1 - Rumancik, Zach (ZWR); Ragoonanan, Jaggan (JR)   
+
+//   * Date: 11/05/2020  
+function nextStep(stepList, atStep, animation){ 
+    const step1Arr = stepList['1']
+    const step2Arr = stepList['2']
+    const step3Arr = stepList['3']
+    const toDo1 = step1Arr.includes(atStep)
+    const toDo2 = step2Arr.includes(atStep)
+    const toDo3 = step3Arr.includes(atStep)
+    clearStep(animation)
+    if (toDo1 === true){
+        if (atStep3 === 10 && animation === 'Three' && atStep3b < 2){
+            atStep3 = 2
+            atStep3b += 1
+        }
+        $(`.${CSS.escape(animation)} .one.${CSS.escape(atStep)}`).addClass('step')
+        if(atStep === 6 && animation === "Two"){
+            $(`.${CSS.escape(animation)} .one.${CSS.escape(atStep)} .notsee`).addClass('see')
+        }  
+        if(atStep === 9 && animation === "Two"){
+            $(`.${CSS.escape(animation)} .one.${CSS.escape(atStep)} .notsee`).addClass('see')
+        }  
+    }    
+    if (toDo2 === true){  
+        $(`.${CSS.escape(animation)} .two.${CSS.escape(atStep)}`).addClass('step see')
+    }
+    if (toDo3 === true){
+        $(`.${CSS.escape(animation)} .three.${CSS.escape(atStep)}`).addClass('step see')   
+    }
+}
+function clearStep(animation) {
+    $(`.${CSS.escape(animation)} .step`).removeClass('step')
+}
+function clearSee(animation) {
+    $(`.${CSS.escape(animation)} .see`).removeClass('see')
+}
 let atStep1 = 0
 let atStep2 = 0
 let atStep3 = 0
+let atStep3b = 0
 const stepList1 = {
     "1":[1,2,3,4,5,6],
     '2':[2,3,4],
@@ -14,14 +61,12 @@ const stepList2 = {
 }
 const stepList3 = {
     "1":[1,2,3,4,5,6,7,8,9,10,11,12],
-    '2':[2,3,5,6,7,10],
+    '2':[2,3,4,5,6,7,10],
     '3':[8,9],
 }
-const buttonOne = document.querySelector('button.One')
-buttonOne.addEventListener("click", ()=> {
+$('button.One').click(()=> {
     atStep1 += 1
     if (atStep1 < 7){
-        console.log(atStep1);
         nextStep(stepList1, atStep1, 'One')
     }else{
         atStep1 = 0
@@ -29,11 +74,9 @@ buttonOne.addEventListener("click", ()=> {
         clearSee('One')
     }
 })
-const buttonTwo = document.querySelector('button.Two')
-buttonTwo.addEventListener("click", ()=> {
+$('button.Two').click(()=> {
     atStep2 += 1
     if (atStep2 < 15){
-        console.log(atStep2);
         nextStep(stepList2, atStep2, 'Two')
     }else{
         atStep2 = 0
@@ -41,54 +84,16 @@ buttonTwo.addEventListener("click", ()=> {
         clearSee('Two')
     }
 })
-function nextStep(stepList, atStep, animation){ 
-    const step1Arr = stepList['1']
-    const step2Arr = stepList['2']
-    const step3Arr = stepList['3']
-    const toDo1 = step1Arr.includes(atStep)
-    const toDo2 = step2Arr.includes(atStep)
-    const toDo3 = step3Arr.includes(atStep)
-    console.log(step1Arr, step2Arr, step3Arr);
-    console.log(toDo1, toDo2, toDo3);
-    clearStep(animation)
-    if (toDo1 === true){
-        const toStep = document.querySelectorAll(`.${CSS.escape(animation)} .one.${CSS.escape(atStep)}`)        
-        toStep.forEach(element => {
-            element.classList.add('step')
-        }); 
-        if(atStep === 6 && animation === "Two"){
-            const stongTrue = document.querySelector(`.${CSS.escape(animation)} .one.${CSS.escape(atStep)} .notsee`)
-            stongTrue.classList.add('see')
-        }  
-        if(atStep === 9 && animation === "Two"){
-            const stongTrue = document.querySelector(`.${CSS.escape(animation)} .one.${CSS.escape(atStep)} .notsee`)
-            stongTrue.classList.add('see')
-        }  
-    }    
-    if (toDo2 === true){
-        const toStep = document.querySelectorAll(`.${CSS.escape(animation)} .two.${CSS.escape(atStep)}`)        
-        toStep.forEach(element => {
-            element.classList.add('step')
-            element.classList.add('see')
-        });   
+$('button.Three').click(()=> {
+    atStep3 += 1
+    if (atStep3 < 12){
+        console.log(atStep3)
+        nextStep(stepList3, atStep3, 'Three')
     }
-    if (toDo3 === true){
-        const toStep = document.querySelectorAll(`.${CSS.escape(animation)} .three.${CSS.escape(atStep)}`)        
-        toStep.forEach(element => {
-            element.classList.add('step')
-            element.classList.add('see')
-        });           
+    else{
+        atStep3 = 0
+        atStep3b = 0
+        clearStep('Three')
+        clearSee('Three')
     }
-}
-function clearStep(animation) {
-    const toRemove = document.querySelectorAll(`.${CSS.escape(animation)} .step`)
-    toRemove.forEach(element => {
-        element.classList.remove('step')
-    })
-}
-function clearSee(animation) {
-    const toRemove = document.querySelectorAll(`.${CSS.escape(animation)} .see`)
-    toRemove.forEach(element => {
-        element.classList.remove('see')
-    })
-}
+})
