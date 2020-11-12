f0a74e45b33cb6c3041e8fe5d7fac18274f6befc
Rumancikz <68083947+Rumancikz@users.noreply.github.com>
2020-11-12 17:33:46 -0500

    more gp4 changes comment done

---
+++ Programming/t1/GP4JJ/script.js
@@ -0,0 +1,13 @@
+//  * Program:  script.js 
+
+//  * purpose: script of Group Project 4
+
+//  * Objectives: 1) Comments; 2) tags ; 3)elements ; 4)documentation 
+
+//  * Class: CSC 3323 - Web Programming; Fall 2020; TR: 645PM - 800PM  
+
+//  * Instructor: Dr. Fazelpour - Copyright © 2020  
+
+//  * Programmer(s): Team 1 - Rumancik, Zach (ZWR); Ragoonanan, Jaggan  
+
+//  * Date: 11/12/2020
