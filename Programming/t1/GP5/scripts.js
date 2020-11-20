73c1b5cf4806634ddbb1a4ddc2f271f43a909517
Rumancikz <68083947+Rumancikz@users.noreply.github.com>
2020-11-19 23:50:10 -0500

    major improvements

--- Programming/t1/GP5/scripts.js
+++ Programming/t1/GP5/scripts.js
@@ -1,11 +1,48 @@
+
+let iss = 'small'
 function toggleCSS() {
     isCSS = !isCSS
     document.styleSheets[0].disabled = isCSS
-    setTimeout(toggleCSS, 50)
 }
-
+function lynnis(imp) {
+    if (imp === key)
+    tompane(8)
+}
+function convolutid(imp){
+    const p = document.querySelectorAll('p')
+    p.forEach(el => {
+        el.innerHTML = imp
+    });
+}
+function alegorica(imp) {
+    convolutid(imp + 'D')
+}
+let op = 'pe'
+function tompane(imp){
+    let temp = imp
+    for (let i = 0; i < 8; i++) {
+        temp += '='        
+    }
+    alegorica(temp)
+}
+function chelyt(val){
+    const val2 = iss+op+per
+    console.log(val2);
+    
+    if (val === (val2)){
+        lynnis(key)
+        document.getElementById('spec').innerHTML = ""
+    }
+}
+let per = 'en'
 let isCSS = false
 const toggle = document.getElementById('toggle')
 toggle.addEventListener('click', () => {
     toggleCSS()  
 })
+const spec = document.getElementById('spec')
+const key = 'thing'
+spec.addEventListener('keypress', () => {
+    chelyt(spec.value)
+})
+
